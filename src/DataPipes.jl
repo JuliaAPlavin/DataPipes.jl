0f96794f436acdc18dbfe84e21f997d4238ee49d
Alexander Plavin <alexander@plav.in>
2022-01-01 00:00:00 +0000

    add abbreviated @p and @pf

--- src/DataPipes.jl
+++ src/DataPipes.jl
@@ -6,4 +6,11 @@ include("utils.jl")
 include("pipe.jl")
 include("data_functions.jl")
 
+module Abbr
+import ..@pipe, ..@pipefunc
+const var"@p" = var"@pipe"
+const var"@pf" = var"@pipefunc"
+export @p, @pf
+end
+
 end
