14f69c66f0cdf27b037bd7070f87c642484f6f1f
Alexander Plavin <alexander@plav.in>
2023-01-01 00:00:00 +0000

    generalize expr walking

--- src/utils.jl
+++ src/utils.jl
@@ -7,12 +7,23 @@ walk(x::Expr, inner, outer) = outer(Expr(x.head, map(inner, x.args)...))
 walk(x::Tuple, inner, outer) = outer(map(inner, x))
 
 
-struct StopWalk
+abstract type WalkModifier end
+
+struct StopWalk <: WalkModifier
+    value
+end
+
+struct ContinueWalk <: WalkModifier
     value
 end
 
 postwalk(f, x) = walk(x, x -> postwalk(f, x), f)
-prewalk(f, x)  = (x_ = f(x); x_ isa StopWalk ? x_.value : walk(x_, x -> prewalk(f, x), identity))
+function prewalk(f, x)
+    x_ = f(x)
+    x_ isa StopWalk ? x_.value :
+    x_ isa ContinueWalk ? prewalk(f, x_.value) :
+    walk(x_, x -> prewalk(f, x), identity)
+end
 
 occursin_expr(needle::Function, haystack) = count_expr(needle, haystack) > 0
 
@@ -20,7 +31,7 @@ function count_expr(needle::Function, haystack)
     cnt = 0
     prewalk(haystack) do ee
         n = needle(ee)
-        n isa StopWalk && return n
+        n isa WalkModifier && return n
         cnt += n::Bool
         ee
     end
